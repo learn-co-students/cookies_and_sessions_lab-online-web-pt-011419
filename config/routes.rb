74ec0c904a3434cecd1b58820cde1d2fdbe4e3c6
NYHolzer <nissan.holzer@gmail.com>
2019-07-15 19:44:27 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'home#home'
-  post '/add', to: 'add#update'
+  root to: 'products#index'
+  post '/add', to: 'products#add'
 end
